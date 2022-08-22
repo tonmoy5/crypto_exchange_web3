b532e3f8cd9962800cc6263187dc100dd8ffcb80
tonmoy5 <tonmoy52532@gmail.com>
2022-08-22 14:21:49 +0600

    Smart contract deployed

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/LMPdPXW-ZW-7h-hVV9cLs-FkEG0zJJ2V
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/LMPdPXW-ZW-7h-hVV9cLs-FkEG0zJJ2V',
+      accounts: ['d3affc51e1556f6c97428a1e1ccd17d8b69864125ef5cf32621fef3c2c6df6a7']
+    }
+  }
+}
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,23 +1,20 @@
 const main = async () => {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Transactons = await hre.ethers.getContractFactory("Transactons");
+  const transaction = await Transactons.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await transaction.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Transactons deployed to: ", transaction.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain();
